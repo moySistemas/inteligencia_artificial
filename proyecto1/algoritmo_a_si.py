35723aec5974bf8175890485f86faafd7d4de0f2
Moises Carrasco <l20120089@morelia.tecnm.mx>
2025-03-12 15:02:28 -0600

    creacion mapa y obstaculos

--- proyecto1/algoritmo_a_si.py
+++ proyecto1/algoritmo_a_si.py
@@ -11,18 +11,18 @@ mapa = np.zeros((10,10),dtype=int)
 
 
 #El formato es: fila, columna
-#En la fila 3 y en la columna del 3 a 6 se asigna un 1
-#En la fila 6 y en la columna del 2 a 7 se asigna un 1
+#En la fila 3 y en la columna del 3 a 6 se asigna un 1 (obstaculo)
+#En la fila 6 y en la columna del 2 a 7 se asigna un 1 (obstaculo)
 mapa[3, 3:7] = 1 
 mapa[6, 2:8] = 1
 
 
 #Inicio(punto de partida), meta(punto final)
 #tupla(fila,columna)
-inicio = (0,0)
+inicio = (7,0)
 meta = (9,9)
-mapa[inicio] = "I"
-mapa[meta] = "M"
+mapa[inicio] = 3
+mapa[meta] = 4
 
 
 print(mapa)
