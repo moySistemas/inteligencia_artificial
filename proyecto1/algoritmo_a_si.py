7d63fe7474b8e9055d012bae4082d709fcfd1f0c
Moises Carrasco <l20120089@morelia.tecnm.mx>
2025-04-14 21:50:52 -0600

    Agregar moviemientos

--- proyecto1/algoritmo_a_si.py
+++ proyecto1/algoritmo_a_si.py
@@ -19,11 +19,17 @@ mapa[6, 2:8] = 1
 
 #Inicio(punto de partida), meta(punto final)
 #tupla(fila,columna)
-inicio = (7,0)
-meta = (9,9)
+#El 3 es inicio y el 4 fin
+inicio = (2,3)
+meta = (7,8)
 mapa[inicio] = 3
 mapa[meta] = 4
 
 
-print(mapa)
 
+def heuristica(inicio,meta):
+	    abs(inicio[0] - meta[0]) + abs(inicio[1] - meta[1])
+
+
+
+print(heuristica(inicio,meta))
